01b1510bb7047f8b79c3b3a2f451db09d6a9ce57
Andrew Maksimov <maksimov.andrew@gmail.com>
2019-10-07 14:30:46 +0300

    rename variables

--- code_to_service.py
+++ code_to_service.py
@@ -12,29 +12,29 @@ class CodeToServiceCommand(sublime_plugin.TextCommand):
     self.edit = edit
     self.view.window().show_input_panel("Service object name:", "", self.get_selected_text, None, None)
 
-  def get_selected_text(self, partial_name):
+  def get_selected_text(self, service_class_name):
     region = self.view.sel()[0]
 
     if not region.empty():
-      partial_code = self.view.substr(region)
-      self.create_partial_file(partial_name, partial_code)
+      service_code = self.view.substr(region)
+      self.create_service_file(service_class_name, service_code)
 
-  def create_partial_file(self, partial_name, partial_code):
+  def create_service_file(self, service_class_name, service_code):
     source                 = self.view.file_name()
     source_path            = os.path.dirname(source)
     rails_view_path        = os.path.dirname(source_path)
-    partial_file_with_path = rails_view_path + '/services/' + partial_name + '.rb'
+    service_file_with_path = rails_view_path + '/services/' + service_class_name + '.rb'
 
-    class_name     = ''.join(x.capitalize() or '_' for x in partial_name.split('_'))
-    new_class_code = 'class ' + class_name + '\n\n' + ' def initialize(args={})\n\n' + ' end\n\n' + partial_code + '\n' + 'end\n'
+    class_name     = ''.join(x.capitalize() or '_' for x in service_class_name.split('_'))
+    new_class_code = 'class ' + class_name + '\n\n' + ' def initialize(args={})\n\n' + ' end\n\n' + service_code + '\n' + 'end\n'
 
-    if not os.path.exists(partial_file_with_path):
-      with open(partial_file_with_path, 'w') as f:
+    if not os.path.exists(service_file_with_path):
+      with open(service_file_with_path, 'w') as f:
         f.write(textwrap.dedent(new_class_code))
 
     self.insert_class_reference(class_name, source)
 
-    self.view.window().open_file(partial_file_with_path)
+    self.view.window().open_file(service_file_with_path)
 
   def insert_class_reference(self, class_name, source):
     region = self.view.sel()[0]
